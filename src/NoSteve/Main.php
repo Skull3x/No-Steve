ce7b3ca77e8d4d434631f54c023ebaa386929fb1
Skullex <jross3740@gmail.com>
2016-03-12 10:28:52 -0500

    Create Main.php

---
+++ src/NoSteve/Main.php
@@ -0,0 +1,17 @@
+<?php
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Color;
+
+class Main extends PluginBase implements Listener {
+
+    public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer();
+    if($player->getName() === "steve"){
+    $p->kick("In order to join this server\nYou must change your name\n \n \nTo something other\nThan Steve!");
+  }
+}
